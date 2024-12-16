39f4d773681b195ce8c06fbbb9de74e53e8f87f8
VICTUS15\52951 <herrera.marin.emily@gmail.com>
2024-12-15 19:36:32 -0600

    bd

--- db.js
+++ db.js
@@ -1,12 +1,17 @@
+require('dotenv').config(); // Importar dotenv para cargar las variables de entorno
+
 const mysql = require('mysql2');
 
+// Crear la conexión utilizando las variables de entorno
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',         
-    password: 'Contraseña123!',  
-    database: 'Naturista' 
+    host: process.env.DB_HOST,       // Host desde el .env
+    user: process.env.DB_USER,       // Usuario desde el .env
+    password: process.env.DB_PASSWORD, // Contraseña desde el .env
+    database: process.env.DB_NAME,   // Base de datos desde el .env
+    port: process.env.DB_PORT        // Puerto desde el .env
 });
 
+// Conectar a la base de datos
 connection.connect((err) => {
     if (err) {
         console.error('Error al conectar a la base de datos:', err.stack);
@@ -14,6 +19,5 @@ connection.connect((err) => {
     }
     console.log('Conectado a la base de datos MySQL.');
 });
-
 
 module.exports = connection;
